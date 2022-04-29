625dcc4d233e5c50d0042e737ae7160bd4b7850b
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2022-04-29 20:57:10 -0300

    feat: initial commit

---
+++ src/main/java/com/bycoderstec/cnabreaderservice/CnabReaderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bycoderstec.cnabreaderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnabReaderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnabReaderServiceApplication.class, args);
+	}
+
+}
